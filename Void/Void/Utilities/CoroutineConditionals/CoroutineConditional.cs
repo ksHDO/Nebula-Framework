82b69580b8010ae64b37ac946b9ed52ac0ea4789
kstamant <kylestamant1@gmail.com>
2018-04-17 11:32:12 -0600

    Added Coroutines back in

---
+++ Void/Void/Utilities/CoroutineConditionals/CoroutineConditional.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void.Utilities.CoroutineConditionals
+{
+    public class CoroutineConditional
+    {
+
+        public virtual bool DoCheck()
+        {
+            return true;
+        }
+
+
+    }
+}
