1461fa916ab8d6ce1028578ad75a89fe1025aeda
kstamant <kylestamant1@gmail.com>
2018-04-17 09:26:05 -0600

    added coroutines

---
+++ Void/CoroutineSamples/Game1.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Void;
+using Void.Utilities;
+using Void.Utilities.CoroutineConditionals;
+
+namespace CoroutineSamples
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Core
+    {
+        //GraphicsDeviceManager graphics;
+        //SpriteBatch spriteBatch;
+        Coroutine coroutine;
+
+        public Game1()
+        {
+            //coroutine = new Coroutine(TestCoroutine());
+            coroutine = CoroutineSystem.BeginCoroutine(TestCoroutine());
+            //graphics = new GraphicsDeviceManager(this);
+            //Content.RootDirectory = "Content";
+        }
+        public IEnumerator<CoroutineConditional> TestCoroutine()
+        {
+            yield return null;
+            Debug.WriteLine("Yielded Null");
+            yield return new WaitFor(3.0f);
+            Debug.WriteLine("Yielded 3.0f");
+
+        }
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            //spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            
+            base.Update(gameTime);
+            CoroutineSystem.Update();
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
